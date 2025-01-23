edb492b861b84616bc2f1e4053b8ad500300b28f
Shalini Mittal <shalini06.mittal@gmail.com>
2025-01-23 17:58:27 +0530

    jar file

--- springbootdocker/src/main/java/com/demo/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.demo;
-
-public class AppConstants {
-
-	public static final String STATUS = "status";
-
-}
--- springbootdocker/src/main/java/com/demo/Status.java
+++
@@ -1,6 +0,0 @@
-package com.demo;
-
-public enum Status {
-
-	SUCCESS, FAILURE
-}
